08801900f96266ee6d4b779fa2fe7e2db5b51873
StereoFlo <StereoFlo@users.github.com>
2020-06-07 21:50:24 +0300

    fix

--- src/controllers/board.go
+++ src/controllers/board.go
@@ -2,11 +2,18 @@ package controllers
 
 import (
 	"app/src/utils"
+	"github.com/gorilla/mux"
 	"goach/models"
 	"net/http"
 )
 
 func List(w http.ResponseWriter, r *http.Request) {
 	data := models.GetList()
+	utils.Respond(w, data)
+}
+
+func GetById(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	data := models.GetBoardById(vars["boardId"])
 	utils.Respond(w, data)
 }
